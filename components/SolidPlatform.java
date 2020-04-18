dcdab6997a034ab42739fa487547dd69191a0a10
LeumasOahz <53974949+LeumasOahz@users.noreply.github.com>
2020-04-18 12:27:02 -0400

    Delete SolidPlatform.java

--- components/SolidPlatform.java
+++
@@ -1,9 +0,0 @@
-package components;
-
-import javafx.scene.paint.Color;
-
-public class SolidPlatform extends Platform {
-    public SolidPlatform(double xPosition, double yPosition, double width, double height) {
-        super(xPosition, yPosition, width, height, Color.rgb(184, 15, 36));
-    }
-}
